6dc88fed1575cec4be9d693af81e315a1f46a83a
Martin Vavrusak <vavmar@gmail.com>
2015-05-25 01:50:40 +0200

    Small changes made implemented deactivation of tool when tab is changed.

--- experimental/lib/main.js
+++ experimental/lib/main.js
@@ -16,23 +16,30 @@ var button = buttons.ActionButton({
 //Enable button when facebook page loads
 pageMod.PageMod({
   include: "*.facebook.com",
-  onAttach: function() {
+  onAttach: activateButton
+});
+
+//Register listeners
+//when clicked on tab
+tabs.on('activate', tabAccessed);
+//when clicked to another tab (tab focus change)
+tabs.on('deactivate', tabDeAccessed);
+
+//button.disabled = true;
+
+//Enable button and set icon
+function activateButton(){
+      console.log();
       button.disabled = false;
       button.icon = {
         "16": "./icon-16.png",
         "32": "./icon-32.png",
         "64": "./icon-64.png"
         };
-  }
-});
+}
 
-//Register listener
-tabs.on('activate', tabAccessed);
-
-//button.disabled = true;
-
-function handleClick(state) {
-  tabs.open("https://www.mozilla.org/");
+//Disable button and set icon
+function deactivateButton(){
   button.icon = {
     "16": "./icon-16_inactive.png",
     "32": "./icon-32_inactive.png",
@@ -41,13 +48,20 @@ function handleClick(state) {
   button.disabled = true;
 }
 
+//When clicked on tool icon
+function handleClick(state) {
+  deactivateButton();
+  tabs.open("https://www.mozilla.org/");
+}
+
 function pageReady(tab){
-    console.log("Page is ready.");
+    console.log("Page: " + tab.title + " is ready.");
+    tab.url
 }
 
 function tabAccessed(tab){
     console.log(tab.readyState);
-    console.log(tab.readyState === 'uninitialized');
+    //console.log(tab.readyState === 'uninitialized');
     console.log("Tab: " + tab.url + " is activated");
     
     if(tab.readyState !== 'complete'){
@@ -56,10 +70,7 @@ function tabAccessed(tab){
     }
 }
 
-function tabDeAccessed(){
-    
-}
-
 function tabDeAccessed(tab){
-    
+    console.log("Leaving tab.");
+    deactivateButton();
 }
